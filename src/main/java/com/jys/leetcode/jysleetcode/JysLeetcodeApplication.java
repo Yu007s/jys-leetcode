64e9846770a92ef84814b7edda0a4fcfb2877f81
宇盛 焦 <jiaoyusheng@cnstrong.cn>
2025-08-12 15:35:55 +0800

    1

---
+++ src/main/java/com/jys/leetcode/jysleetcode/JysLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.jys.leetcode.jysleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JysLeetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JysLeetcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jys/leetcode/jysleetcode/JysLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jys.leetcode.jysleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JysLeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
